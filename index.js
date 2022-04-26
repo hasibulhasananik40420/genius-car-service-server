2622816c9d5045c5b2a149e332ff6f1a2e91af9d
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-04-26 17:17:43 +0600

    add done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -9,6 +10,25 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+function jwtVerify(req,res,next){
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.EXCESS_TOKEN , (err ,decoded )=>{
+        if(err){
+            return res.status(403).send({message:'Forbiden access'})
+        }
+        console.log('decoded' , decoded)
+        req.decoded =decoded
+        next()
+    })
+    // console.log('jwt',authHeader)
+   
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kz7ts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -18,7 +38,20 @@ async function run() {
     try {
         await client.connect()
         const seviceCollection = client.db('geniuscar').collection('service')
+        const orderCollection = client.db('geniuscar').collection('order')
 
+    //   Auth
+         app.post('/login' , async(req,res)=>{
+             const user = req.body 
+             const accessToken = jwt.sign(user , process.env.EXCESS_TOKEN ,{
+                 expiresIn: '1d'
+             })
+            //  console.log(accessToken);
+             res.send({accessToken})
+         })
+       
+
+        //service api
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = seviceCollection.find(query)
@@ -48,6 +81,30 @@ async function run() {
             res.send(result)
         })
 
+        //oder collection api
+
+       app.get('/order',jwtVerify ,async(req,res)=>{
+            const decodedEmail = req.decoded.email
+           const email = req.query.email
+           if(email===decodedEmail){
+            const query ={email:email}
+            const cursor = orderCollection.find(query)
+            const orders =await cursor.toArray()
+            res.send(orders)
+           }
+           else{
+             res.status(403).send({message:'Forbiden access'})
+           }
+       })
+
+
+        app.post('/order', async(req,res)=>{
+            const order = req.body 
+            const result =await orderCollection.insertOne(order)
+            res.send(result)
+
+        })
+
     }
 
     finally {
@@ -64,3 +121,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
+
+
